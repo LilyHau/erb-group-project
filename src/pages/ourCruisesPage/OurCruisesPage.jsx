b69b0bf526bfa288ac67c0f8dc00abdeadf74fc3
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-16 15:29:08 +0800

    Fix about page

--- src/pages/ourCruisesPage/OurCruisesPage.jsx
+++ src/pages/ourCruisesPage/OurCruisesPage.jsx
@@ -1,5 +1,12 @@
+import Image from "../../assets/Cruises/cruisesum.png";
 const OurCruisesPage = () => {
-  return <div>OurCruisesPage</div>;
+  return (
+    <div>
+      <div>
+        <img src={Image} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default OurCruisesPage;
